863c330c5e1892faaa9578dc3d39190e9d75ea5b
rcanand <rcanand@gmail.com>
2016-10-25 19:54:11 +0530

    grid computation

--- rb/blackbox/lib/blackbox.rb
+++ rb/blackbox/lib/blackbox.rb
@@ -1,8 +1,39 @@
 #!/usr/bin/env ruby -wU
+require 'matrix'
 class Blackbox
-    def initialize grid_size, num_atoms
-        raise ArgumentError.new("grid_size and num_atoms must be integers") unless grid_size.instance_of?(Fixnum) && num_atoms.instance_of?(Fixnum)
-        raise ArgumentError.new("grid_size must be between 2 and 50 inclusive") unless grid_size > 0 && grid_size < 51
-        raise ArgumentError.new("num_atoms must be between 0 and grid_size inclusive") unless num_atoms >= 0 && num_atoms <= grid_size
+    attr_reader :dimension, :num_atoms, :outer_dimension, :outer_grid_area, :grid, :square_numbers_from_positions
+
+    def initialize dimension, num_atoms
+        raise ArgumentError.new("dimension and num_atoms must be integers") unless dimension.instance_of?(Fixnum) && num_atoms.instance_of?(Fixnum)
+        raise ArgumentError.new("dimension must be between 2 and 50 inclusive") unless dimension > 0 && dimension < 51
+        raise ArgumentError.new("num_atoms must be between 0 and dimension inclusive") unless num_atoms >= 0 && num_atoms <= dimension
+        @dimension = dimension
+        @outer_dimension = dimension + 2
+        @outer_grid_area = @outer_dimension * @outer_dimension
+        @square_numbers_from_positions = {}
+        @outer_dimension.times do |row|
+            @outer_dimension.times do |column|
+                @square_numbers_from_positions[[row, column]] = get_square_number_from_position(row, column)
+            end
+        end
+
+        @num_atoms = num_atoms
+    end
+
+    def get_square_number_from_position row, column
+        extremes = [0, @outer_dimension - 1]
+        if(extremes.include?(row) && extremes.include?(column)) # corners
+            return 0
+        elsif(row == 0) # top edge
+            return column
+        elsif(column == @outer_dimension - 1) # right edge
+            return (@dimension + row)
+        elsif(row == @outer_dimension - 1) # bottom edge
+            return (2*@dimension + (@dimension - column + 1))
+        elsif(column == 0) # left edge
+            return (3*@dimension + dimension - row + 1)
+        else # inner square
+            return (4 * @dimension + (row - 1) * @dimension + column)
+        end
     end
 end
--- rb/blackbox/test/test_blackbox.rb
+++ rb/blackbox/test/test_blackbox.rb
@@ -1,9 +1,23 @@
+# require 'byebug'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/blackbox.rb'
 
 
 class TestBlackbox < Minitest::Test
+    PARAMETERS = [
+        [1,0],
+        [1,1],
+        [8,3],
+        [10,0],
+        [10,1],
+        [10,10],
+        [50,0],
+        [50,1],
+        [50,10],
+        [50,50]
+    ]
+
     def test_bb_class
         Blackbox
     end
@@ -33,7 +47,7 @@ class TestBlackbox < Minitest::Test
         Blackbox.new 50, 50
     end
 
-    def test_grid_size_must_be_between_1_and_50_inclusive
+    def test_dimension_must_be_between_1_and_50_inclusive
         assert_raises(ArgumentError) {Blackbox.new -1, -1}
         assert_raises(ArgumentError) {Blackbox.new 0, -1}
         assert_raises(ArgumentError) {Blackbox.new 0, 0}
@@ -41,7 +55,7 @@ class TestBlackbox < Minitest::Test
         assert_raises(ArgumentError) {Blackbox.new 100, 1}
     end
     
-    def test_bb_num_atoms_must_be_between_0_and_grid_size_inclusive
+    def test_bb_num_atoms_must_be_between_0_and_dimension_inclusive
         assert_raises(ArgumentError) {Blackbox.new 1, -1}
         assert_raises(ArgumentError) {Blackbox.new 1, 2}
         assert_raises(ArgumentError) {Blackbox.new 1, 10}
@@ -53,5 +67,96 @@ class TestBlackbox < Minitest::Test
         assert_raises(ArgumentError) {Blackbox.new 50, 100}
     end
 
+    def test_bb_returns_dimension
+        PARAMETERS.each do |dimension, num_atoms|
+            bb = Blackbox.new(dimension, num_atoms)
+            assert_equal(dimension, bb.dimension)
+        end
+    end
 
+    def test_bb_returns_num_atoms
+        PARAMETERS.each do |dimension, num_atoms|
+            bb = Blackbox.new(dimension, num_atoms)
+            assert_equal(num_atoms, bb.num_atoms)
+        end
+    end
+
+    def test_bb_returns_correct_outer_dimension
+        PARAMETERS.each do |dimension, num_atoms|
+            bb = Blackbox.new(dimension, num_atoms)
+            assert_equal(dimension + 2, bb.outer_dimension)
+        end
+    end
+
+    def test_bb_returns_correct_outer_grid_area
+        PARAMETERS.each do |dimension, num_atoms|
+            bb = Blackbox.new(dimension, num_atoms)
+            assert_equal(bb.outer_dimension*bb.outer_dimension, bb.outer_grid_area)
+        end
+    end 
+
+    def test_square_numbers_from_positions
+        bb_1 = Blackbox.new(1, 0)
+        
+        assert_equal(0, bb_1.get_square_number_from_position(0, 0))
+        assert_equal(1, bb_1.get_square_number_from_position(0, 1))
+        assert_equal(0, bb_1.get_square_number_from_position(0, 2))
+        assert_equal(4, bb_1.get_square_number_from_position(1, 0))
+        assert_equal(5, bb_1.get_square_number_from_position(1, 1))
+        assert_equal(2, bb_1.get_square_number_from_position(1, 2))
+        assert_equal(0, bb_1.get_square_number_from_position(2, 0))
+        assert_equal(3, bb_1.get_square_number_from_position(2, 1))
+        assert_equal(0, bb_1.get_square_number_from_position(2, 2))
+
+        bb_2 = Blackbox.new(2,0)
+        assert_equal(0, bb_2.get_square_number_from_position(0, 0))
+        assert_equal(1, bb_2.get_square_number_from_position(0, 1))
+        # byebug
+        assert_equal(2, bb_2.get_square_number_from_position(0, 2))
+        assert_equal(0, bb_2.get_square_number_from_position(0, 3))
+        assert_equal(8, bb_2.get_square_number_from_position(1, 0))
+        assert_equal(9, bb_2.get_square_number_from_position(1, 1))
+        assert_equal(10, bb_2.get_square_number_from_position(1, 2))
+        assert_equal(3, bb_2.get_square_number_from_position(1, 3))
+        assert_equal(7, bb_2.get_square_number_from_position(2, 0))
+        assert_equal(11, bb_2.get_square_number_from_position(2, 1))
+        assert_equal(12, bb_2.get_square_number_from_position(2, 2))
+        assert_equal(4, bb_2.get_square_number_from_position(2, 3))                
+        assert_equal(0, bb_2.get_square_number_from_position(3, 0))
+        assert_equal(6, bb_2.get_square_number_from_position(3, 1))
+        assert_equal(5, bb_2.get_square_number_from_position(3, 2))
+        assert_equal(0, bb_2.get_square_number_from_position(3, 3))
+    end
+
+    def test_square_numbers_from_positions_hash
+        bb_1 = Blackbox.new(1, 0)
+        
+        assert_equal(bb_1.get_square_number_from_position(0,0), bb_1.square_numbers_from_positions[[0,0]])
+        assert_equal(bb_1.get_square_number_from_position(0,1), bb_1.square_numbers_from_positions[[0,1]])
+        assert_equal(bb_1.get_square_number_from_position(0,2), bb_1.square_numbers_from_positions[[0,2]])
+        assert_equal(bb_1.get_square_number_from_position(1,0), bb_1.square_numbers_from_positions[[1,0]])
+        assert_equal(bb_1.get_square_number_from_position(1,1), bb_1.square_numbers_from_positions[[1,1]])
+        assert_equal(bb_1.get_square_number_from_position(1,2), bb_1.square_numbers_from_positions[[1,2]])
+        assert_equal(bb_1.get_square_number_from_position(2,0), bb_1.square_numbers_from_positions[[2,0]])
+        assert_equal(bb_1.get_square_number_from_position(2,1), bb_1.square_numbers_from_positions[[2,1]])
+        assert_equal(bb_1.get_square_number_from_position(2,2), bb_1.square_numbers_from_positions[[2,2]])
+
+        bb_2 = Blackbox.new(2,0)
+        assert_equal(bb_2.get_square_number_from_position(0,0), bb_2.square_numbers_from_positions[[0,0]])
+        assert_equal(bb_2.get_square_number_from_position(0,1), bb_2.square_numbers_from_positions[[0,1]])
+        assert_equal(bb_2.get_square_number_from_position(0,2), bb_2.square_numbers_from_positions[[0,2]])
+        assert_equal(bb_2.get_square_number_from_position(0,3), bb_2.square_numbers_from_positions[[0,3]])
+        assert_equal(bb_2.get_square_number_from_position(1,0), bb_2.square_numbers_from_positions[[1,0]])
+        assert_equal(bb_2.get_square_number_from_position(1,1), bb_2.square_numbers_from_positions[[1,1]])
+        assert_equal(bb_2.get_square_number_from_position(1,2), bb_2.square_numbers_from_positions[[1,2]])
+        assert_equal(bb_2.get_square_number_from_position(1,3), bb_2.square_numbers_from_positions[[1,3]])
+        assert_equal(bb_2.get_square_number_from_position(2,0), bb_2.square_numbers_from_positions[[2,0]])
+        assert_equal(bb_2.get_square_number_from_position(2,1), bb_2.square_numbers_from_positions[[2,1]])
+        assert_equal(bb_2.get_square_number_from_position(2,2), bb_2.square_numbers_from_positions[[2,2]])
+        assert_equal(bb_2.get_square_number_from_position(2,3), bb_2.square_numbers_from_positions[[2,3]])                
+        assert_equal(bb_2.get_square_number_from_position(3,0), bb_2.square_numbers_from_positions[[3,0]])
+        assert_equal(bb_2.get_square_number_from_position(3,1), bb_2.square_numbers_from_positions[[3,1]])
+        assert_equal(bb_2.get_square_number_from_position(3,2), bb_2.square_numbers_from_positions[[3,2]])
+        assert_equal(bb_2.get_square_number_from_position(3,3), bb_2.square_numbers_from_positions[[3,3]])
+    end
 end
